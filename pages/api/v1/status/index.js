4e3d26492b99e985c0c199719aeb36dd85651528
Ismael Monteiro <87835141+Ismaelthe10@users.noreply.github.com>
2026-08-08 00:23:08 +0000

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,6 +5,7 @@ export default async function status(request, response) {
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseVersionValueNumber = databaseVersionValue.split(" ")[0];
 
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
@@ -25,7 +26,7 @@ export default async function status(request, response) {
 
     dependencies: {
       database: {
-        version: databaseVersionValue,
+        version: databaseVersionValueNumber,
         max_connections: parseInt(databaseMaxConnectionsValue),
         opened_connections: databaseOpenedConnectionsValue,
       },
